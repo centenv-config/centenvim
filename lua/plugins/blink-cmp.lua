2e77e250a563f60c5c247505803f6f19c52bc50c
vincent <vincent.nguyennf@gmail.com>
2026-05-17 22:47:09 -0600

    Fixed column layout bug

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -27,10 +27,20 @@ return {
           draw = {
             treesitter = { "lsp" },
             columns = {
-              { "kind_icon", "kind", gap = 1 },
-              { "label", "label_description", gap = 2 },
-              { "source_name" }
-            }
+              -- { "label", "label_description", gap = 2 },
+              { "kind_icon", "label", gap = 1 },
+              { "kind", "source_name", gap = 2 },
+            },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
           },
         },
         documentation = {
@@ -58,25 +68,6 @@ return {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
       fuzzy = { implementation = "prefer_rust_with_warning" },
-      completion = {
-        menu = {
-          draw = {
-            -- We don't need label_description now because label and label_description are already
-            -- combined together in label by colorful-menu.nvim.
-            columns = { { "kind_icon" }, { "label", gap = 1 } },
-            components = {
-              label = {
-                text = function(ctx)
-                  return require("colorful-menu").blink_components_text(ctx)
-                end,
-                highlight = function(ctx)
-                  return require("colorful-menu").blink_components_highlight(ctx)
-                end,
-              },
-            },
-          },
-        },
-      },
     })
   end
 }
